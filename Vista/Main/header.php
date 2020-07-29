1fc97cf34de4b2c7a1c4a287ba5bae3c702a58a2
Mateo Epalza <mateoepalzaramirez@gmail.com>
2020-07-28 21:25:53 -0500

    Se realizo el diseÃo de la pagina principal
    * Se realizo el diseÃo de la pagina principal
    * Se realizo el efecto de las imagenes

---
+++ Vista/Main/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Prueba pagina principal</title>
+        <meta charset="utf-8">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+        <link rel="stylesheet" href="Static/css/index.css">
+    </head>
+    <body>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+    session_start();
+    date_default_timezone_set('America/Bogota');
+    
+    $pid = null;
+
+    if(isset($_GET['cerrarSesion'])){
+        session_destroy();
+        $_SESSION = [];
+    }
+    if(isset($_GET['pid'])){
+        $pid = base64_decode($_GET['pid']);
+    }
+
+    include "Vista/Main/header.php";
+    
+    $enter = Array('Vista/Auth/autenticar.php', 'Vista/Auth/clienteActivarCuenta.php');
+
+    if(isset($pid)){
+
+        if(isset($_SESSION['id'])){
+            if($_SESSION['rol'] == 1){
+                include "Vista/Administrador/navAdministrador.php";
+                include $pid;
+            }else if($_SESSION['rol'] == 2){
+                include "Vista/Cliente/navCliente.php";
+                include $pid;
+                include "Vista/Cliente/footerCliente.php";
+            }else if($_SESSION['rol'] == 3){
+                include "Vista/Inventarista/navInventarista.php";
+                include $pid;
+            }else{
+                include "Vista/Main/mainPage.php";/*Toca cambiarlo*/
+            }
+        }else if(in_array($pid, $enter)){
+            include $pid;
+        }else{
+            header("Location: index.php");
+        }
+
+    }else{
+        include "Vista/Main/mainPage.php";
+        
+        if(isset($_SESSION['id'])){
+            session_destroy();
+            $_SESSION = [];
+        }
+        
+    }
+    
+    include "Vista/Main/footer.php";
+?>
