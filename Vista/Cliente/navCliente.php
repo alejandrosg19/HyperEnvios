5ccf732beb3e59f9c367b383bfdb0c77865d5115
Mateo Epalza <mateoepalzaramirez@gmail.com>
2020-08-02 13:24:22 -0500

    Cliente creacion de orden e items
    * Creacion del formulario de creacion de orden en cliente
    * Creacion de la clase de orden

--- Vista/Cliente/navCliente.php
+++ Vista/Cliente/navCliente.php
@@ -63,10 +63,10 @@
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Apps
+                            Orden
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="index.php?pid=<?php echo base64_encode("Vista/Cliente/crearOrden.php") ?>">Generar Orden</a>
                             <a class="dropdown-item" href="#">Another action</a>
                             <a class="dropdown-item" href="#">Something else here</a>
                         </div>
