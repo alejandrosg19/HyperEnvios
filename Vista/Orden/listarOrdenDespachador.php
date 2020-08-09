a7acb8f4088df6f86cab00cdce22607f0e286a28
Mateo Epalza <mateoepalzaramirez@gmail.com>
2020-08-08 21:00:06 -0500

    Bubble fixed

--- Vista/Orden/listarOrdenDespachador.php
+++ Vista/Orden/listarOrdenDespachador.php
@@ -259,21 +259,6 @@
             });
         });
 
-        /*
-         *
-         */
-        $('.table').on('change', '.select-estado', function() {
-            json = {
-                "idOrden": $(this).data('id'),
-                "estado": $(this).val(),
-            };
-            $.post("indexAJAX.php?pid=<?php echo base64_encode("Vista/Orden/Ajax/updateEstadoOrden.php") ?>", json, function(data) {
-                console.log(data);
-                res = JSON.parse(data);
-                crearAlert(res.status, res.msj);
-            });
-        });
-
     });
 
     function createComments(allData){
@@ -359,12 +344,13 @@
                     <td>${data[3]}</td>
                     <td>${data[4]}</td>
                     <td>${data[5]}</td>
+                    <td>${data[7]}</td>
                     <td>
                         <select class='select-estado form-control' data-id='${data[0]}'>
-                            <option value='3' ${(data[7] == 3)?"selected hidden":"hidden"}>Recogido</option>
-                            <option value='5' ${(data[7] == 3 ? "" : (data[7] == 5 ? "selected hidden" : "hidden"))} >Recibido</option>
-                            <option value='6' ${(data[7] == 5 ? "" : (data[7] == 6 ? "selected hidden" : "hidden"))} >En Bodega</option>
-                            <option value='7' ${(data[7] == 6 ? "" : (data[7] == 7 ? "selected disabled" : "hidden"))} >Despachado</option>
+                            <option value='1' ${(data[7] == "Recogido")?"selected":"hidden"}>Recogido</option>
+                            <option value='0' ${(data[7] == "Recogido" ? "" : (data[7] == "Recibido" ? "selected" : "hidden"))} >Recibido</option>
+                            <option value='0' ${(data[7] == "Recibido" ? "" : (data[7] == "En Bodega" ? "selected" : "hidden"))} >En Bodega</option>
+                            <option value='0' ${(data[7] == "En Bodega" ? "" : (data[7] == "Despachado" ? "selected" : "hidden"))} >Despachado</option>
                         </select>
                     </td>
                     <td style='display:flex; justify-content:center;'>
@@ -421,7 +407,7 @@
 
     function crearAlert(status, msj) {
         let className = "";
-        console.log("EN ALERTAA");
+
         if (status) {
             className = "alert-success";
         } else {
