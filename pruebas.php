68a04fc4994df78597e956ecced76fabbfbd17f9
Santiago Gonzalez <saagonzalezp@correo.udistrital.edu.co>
2020-08-24 13:51:14 -0500

    Pruebas Finales

---
+++ pruebas.php
@@ -0,0 +1,333 @@
+<?php
+$arrayClientes = array(
+    "Jairo Antonio Escobar Vela",
+    "Daniel Omar Marein Ratto",
+    "Victor Manuel Panadero Monroy",
+    "Maximiliano Perez Ramirez",
+    "Jose Orlando Quinche Silva",
+    "Hector Alberto Diaz Cajamarca",
+    "Fabio Leonel Jimenez Velasquez",
+    "Amadeo Chacon Gonzalez",
+    "Jose Antonio Carvajal Garzon",
+    "Jose Erney Paramo Ortiz",
+    "Edgar Hernando Moreno Rodriguez",
+    "Mario Peraza Castiblanco",
+    "Eduardo Naranjo Martinez",
+    "Marco Alexander Castillo Gonzalez",
+    "Jose Guillermo Zea Figueroa",
+    "Ricardo Ocampo Luna",
+    "Carlos Andres Sanchez",
+    "Luis Eutimio Grimaldos Rodriguez",
+    "Luis Jacobo Arguello",
+    "Rigoberto Florez Gonzalez",
+    "Filemon Moncaleano Lozano",
+    "Rodolfo Lozano Hernandez",
+    "Carlos Humberto Castro Rodriguez",
+    "Vilder Ferneides Miranda Caly",
+    "Humberto Romero Alvarez",
+    "Javier Antonio Higuita Fierro",
+    "Jose Edilberto Rodriguez Cruz",
+    "Gustavo Aleczi Rodriguez Bayona",
+    "Jose Nelson Castelblanco Romero",
+    "Bernardo Angel Lopez Gaviria",
+    "Jorge Alberto Giraldo Mejia",
+    "Diego Fernando Loaiza Pinzon",
+    "Jorge Antonio Aguilar Murcia",
+    "Carlos Eduardo Ayala Barreto",
+    "Nilson Gomez Rivera",
+    "Rafael Vargas Herrera",
+    "Rolando Ismeth Mora Gaitan",
+    "Omar Eusebio Rincon Castiblanco",
+    "Victor Gabriel Campos Fonseca",
+    "Alberto Avella Camargo",
+    "Hugo Fernando Uni Claros",
+    "Edelmer Sanchez Cerquera",
+    "Farid Rojas Ramirez",
+    "Cesar Alberto Rey Delgado",
+    "Antonio Pablo Ruiz Rojas",
+    "Jeremias Silva OrduÃ±a",
+    "Celso Devia",
+    "Jorge Mauricio Lopez Hoyos",
+    "Robinson Rios Noriega",
+    "Sergio De Jesus Bejarano",
+    "Carlos Eduardo Baron CastaÃ±eda",
+    "Tito Lopez",
+    "Anibal Cappella Zarate",
+    "Jose Plinio Soler Valero",
+    "Desiderio De Jesus Castelblanco",
+    "Jorge Enrique Sotomonte",
+    "Luis Enrique Lopez Forero",
+    "Edgar Noel Torres",
+    "Yesid Alfonso Leon Monroy",
+    "Juan De Jesus Gomez",
+    "Jose Alcides Alba Laverde",
+    "Omar Porras Ortiz",
+    "Omar Ignacio Vanegas Guzman",
+);
+
+$arrayDirecciones = array(
+    "CLL 12A # 71C - 60 T. 5 AP. 1120",
+    "DG 91 # 9A - 71 AP. 601",
+    "CRA 121 # 128B 52 IN 9 BL 6 AP 201",
+    "CLL 7B # 12A - 03",
+    "CRA 19 # 11 - 18",
+    "CLL 22F # 103B - 15",
+    "CRA 107C # 70 - 63",
+    "CRA 71C # 75A - 24",
+    "CRA 104 # 15A - 38 IN. 10 CS. 3",
+    "CRA 81B # 6B-85 CS 127 ET 4",
+    "CRA 98B # 65 - 09 SUR",
+    "CRA 92B # 42 - 62 SUR",
+    "CRA 98B # 65 - 09 SUR CS 30",
+    "CRA 11 # 140 - 41 T. 4 AP. 706",
+    "CLL 21 SUR # 9F - 03 ESTE",
+    "CRA 53B # 49 - 14",
+    "CRA 126 # 120 - 65 CS. 105",
+    "TV 77 # 7A - 16",
+    "CRA 19A # 63B - 14 AP.402",
+    "CRA77 # 18 - 51 T. 1 AP. 1204",
+    "CLL 57 H SUR # 71D - 45 IN. 2 AP. 201",
+    "CLL 25C # 2A - 09",
+    "CRA 93D 71 49 SUR CS 117",
+    "CRA 88C # 69B - 20 SUR",
+    "CLL 151A # 45 - 41 T. 2 AP. 204",
+    "CLL 17 # 22 18",
+    "CLL 9F # 16B - 21",
+    "CLL 2 # 91C - 85 CS. 5 33",
+    "CRA 87 # 48 - 01 SUR IN. 3",
+    "CRA 73H # 62G - 04 SUR",
+    "CLL 152 # 58 - 51 IN. 3 AP. 203",
+    "CLL 19 # 98 - 69",
+    "CRA 42 # 1C - 17",
+    "CRA 16 ESTE # 36 - 95",
+    "CRA 35 # 11 - 16",
+    "CRA 88F # 38 - 22 SUR",
+    "CRA 72D # 53A - 64 SUR",
+    "DG 30L # 7D - 29",
+    "CLLE 14 # 7 - 29",
+    "AV 13 # 8A - 184 T. 11 AP. 203",
+    "CRA 89 BIS # 42 - 56 SUR",
+    "CRA 87B BIS # 56C - 37 SUR",
+    "CRA 4J # 52A - 83 SUR",
+    "CRA 53G # 49 - 14",
+    "CRA 97B BIS # 38 - 35",
+    "CRA 8G BIS # 164A - 63",
+    "CLL 10 # 9 - 50 SUR",
+    "CLL 55A SUR # 64 - 31",
+    "CRA 70C # 68 - 73 IN. 13 AP. 502",
+    "CRA 11 BIS # 124A - 66 AP. 701",
+    "CLL 141 # 7B - 50 AP. 513",
+    "CLL 48P BIS C SUR # 3 - 34 IN. 4 AP. 303",
+    "CRA 69B # 24B - 55 IN. 7 AP. 203",
+    "CLL 48J BIS # 5A - 12 SUR",
+    "CLL 17 SUR # 29C - 40",
+    "CLL 45 SUR # 72U - 50",
+    "CLL 45A SUR # 89B - 42 CS. 5",
+    "CLL 92 BIS SUR # 14I - 57",
+    "TV 93 # 22D - 10",
+    "DG 49 SUR # 87 - 26",
+    "DG 39A # 17 - 66 M.212",
+    "DG 76 SUR # 78 - 85",
+    "TV 1D # 77 - 61 SUR",
+);
+$arrayComentarios = array(
+    "Paquete delicado",
+    "Traer caja",
+    "Paquete fragil",
+    "Pesado es dificil de cargar",
+    "Electrodomestico",
+    "Traer carreta de carga",
+    "Llegar a tiempo",
+    "Por favor golpear no timbrar",
+    "Liquido inflamable tener cuidado",
+    "Parquear en el garaje"
+);
+$arrayCCE1 = array(
+    "En camino",
+    "Retraso por trafico en la via",
+    "Retraso por accidente en la vida",
+    "Retraso por marchas",
+    "Retraso por averia mecanica"
+);
+$arrayCCE2 = array(
+    "Paquete mal empacado",
+    "Paquete rasgado",
+    "Paquete golpeado",
+    "Paquete humedo",
+    "Paquete abierto",
+);
+$arrayCDE1 = array(
+    "Paquete mal empacado",
+    "Paquete rasgado",
+    "Paquete golpeado",
+    "Paquete humedo",
+    "Paquete abierto",
+);
+$arrayCDE2 = array(
+    "Paquete estropeado",
+    "Paquete mal embalado",
+    "Paquete mal ubicado",
+    "Paquete humedo",
+    "Paquete mal apilado"
+);
+$arrayCDE3 = array(
+    "En retraso",
+    "Paquete no revisado",
+);
+
+$info = "";
+$citas = "";
+$estadoCliente = "";
+$comentarioCliente = "";
+$envio = "";
+$estadoConductor = "";
+$comentarioConductor = "";
+$estadoDespachador = "";
+$comentarioDespachador = "";
+
+
+$arraydia = array(1, 1);
+$mes = 6;
+$comentario = 1;
+$contEnvio = 0;
+$idEnvio = 1;
+$idEstadoCOnductor = 1;
+$idDespachador = 1;
+$idEstadoDespachador = 1;
+for ($i = 1; $i <= 63; $i++) {
+    $fecha = "2020-0" . $mes . "-" . ($arraydia[1] < 10 ? "0" : "") . "" . $arraydia[1] . " 12:26:19";
+    $fechaEstimacion = "2020-0" . (($arraydia[1] + 6) > 28 ? $mes + 1 : $mes) . "-" . (($arraydia[1] + 6) < 10 ? "0" : "") . "" . (($arraydia[1] + 6) > 28 ? (($arraydia[1] + 6) - 28) : ($arraydia[1] + 6));
+    $valorFecha = rand(2, 3);
+    $fechaLlegada = "2020-0" . (($arraydia[1] + $valorFecha) >= 28 ? $mes + 1 : $mes) . "-" . (($arraydia[1] + $valorFecha) < 10 ? "0" : "") . "" . (($arraydia[1] + $valorFecha) > 28 ? (($arraydia[1] + $valorFecha) - 28) : ($arraydia[1] + $valorFecha));
+    $idCliente = rand(1, 3);
+    $idDespachador = rand(1, 3);
+    $numero = rand(3000000000, 3999999999);
+
+    #ENVIO
+    $contEnvio++;
+    $fechaSalida = "2020-0" . (($arraydia[1] + 6) > 28 ? $mes + 1 : $mes) . "-" . (($arraydia[1] + 5) < 10 ? "0" : "") . "" . (($arraydia[1] + 5) > 28 ? (($arraydia[1] + 5) - 28) : ($arraydia[1] + 5));
+    if ($contEnvio == 3) {
+        $cond2 = rand(1, 3);
+        $envio .= "INSERT INTO envio VALUES('" . $idEnvio . "','" . $fechaSalida . "','" . $cond2 . "'); <br>";
+        $contEnvio = 1;
+        $idEnvio++;
+    }
+    
+
+    $z = $i - 1;
+    #cita
+    $citas = $citas . "INSERT INTO cita VALUES('" . $i . "','" . $fechaLlegada . "','" . $idDespachador . "');<br>";
+    #orden
+    $info = $info . "INSERT INTO orden VALUES('" . $i . "','" . $fecha . "','" . $fechaEstimacion . "','" . $arrayDirecciones[$z] . "','" . $arrayClientes[$z] . "','" . $numero . "','" . $fechaLlegada . "','" . $idCliente . "','" . $i . "','" . $idEnvio . "','" . $idDespachador . "'); <br>";
+    #estadoCliente
+    $estadoCliente = $estadoCliente . "INSERT INTO estadoCliente VALUES('" . $i . "','" . $fecha . "','1','" . $i . "','" . $idCliente . "'); <br>";
+    #comentario cliente
+    $bandEstado = rand(1, 2);
+    if ($bandEstado == 1) {
+        $cantFecha = rand(0, 1);
+        $AC = rand(0, 9);
+        $hora = rand(13, 23);
+        $minutos = rand(1, 59);
+        $segundos = rand(1, 59);
+        $fechaComentario = "2020-0" . (($arraydia[1] + $cantFecha) >= 28 ? $mes + 1 : $mes) . "-" . (($arraydia[1] + $cantFecha) < 10 ? "0" : "") . "" . (($arraydia[1] + $cantFecha) > 28 ? (($arraydia[1] + $cantFecha) - 28) : ($arraydia[1] + $cantFecha)) . " " . $hora . ":" . $minutos . ":" . $segundos;
+        $comentarioCliente = $comentarioCliente . "INSERT INTO comentarioCliente('" . $comentario . "','" . $fechaComentario . "','" . $arrayComentarios[$AC] . "','" . $i . "'); <br>";
+        $comentario++;
+    }
+
+    /**CONDUCTOR */
+    #estadoConductor
+    $hora = 8;
+    $minutos = rand(1, 59);
+    $segundos = rand(1, 59);
+    $fechaEC = "2020-0" . (($arraydia[1] + $valorFecha) > 28 ? $mes + 1 : $mes) . "-" . (($arraydia[1] + $valorFecha) < 10 ? "0" : "") . "" . (($arraydia[1] + $valorFecha) > 28 ? (($arraydia[1] + $valorFecha) - 28) : ($arraydia[1] + $valorFecha)) . " 0" . $hora . ":" . $minutos . ":" . $segundos;
+    $estadoConductor .= "INSERT INTO estadoConductor VALUES('" . $idEstadoCOnductor . "','" . $fechaEC . "','2','" . $i . "','" . $idDespachador . "');<br>";
+
+    /**comentarioConductor1 */
+    $bandEstado = rand(1, 2);
+    if ($bandEstado == 1) {
+        $AC = rand(0, 4);
+        $comentarioConductor = $comentarioConductor . "INSERT INTO comentarioConductor('','" . $fechaEC . "','" . $arrayCCE1[$AC] . "','" . $idEstadoCOnductor . "'); <br>";
+    }
+
+    $idEstadoCOnductor++;
+    $hora = rand(10, 16);
+    $minutos = rand(1, 59);
+    $segundos = rand(1, 59);
+    $fechaEC = "2020-0" . (($arraydia[1] + $valorFecha) > 28 ? $mes + 1 : $mes) . "-" . (($arraydia[1] + $valorFecha) < 10 ? "0" : "") . "" . (($arraydia[1] + $valorFecha) > 28 ? (($arraydia[1] + $valorFecha) - 28) : ($arraydia[1] + $valorFecha)) . " " . $hora . ":" . $minutos . ":" . $segundos;
+    $estadoConductor .= "INSERT INTO estadoConductor VALUES('" . $idEstadoCOnductor . "','" . $fechaEC . "','3','" . $i . "','" . $idDespachador . "');<br>";
+
+
+    /**comentarioConductor2 */
+    $bandEstado = rand(1, 2);
+    if ($bandEstado == 1) {
+        $AC = rand(0, 4);
+        $comentarioConductor = $comentarioConductor . "INSERT INTO comentarioConductor('','" . $fechaEC . "','" . $arrayCCE2[$AC] . "','" . $idEstadoCOnductor . "'); <br>";
+    }
+    $idEstadoCOnductor++;
+
+
+    /**DESPACHADOR */
+    #estadodESPACHADOR1
+    $fechaED = "2020-0" . (($arraydia[1] + $valorFecha) > 28 ? $mes + 1 : $mes) . "-" . (($arraydia[1] + $valorFecha) < 10 ? "0" : "") . "" . (($arraydia[1] + $valorFecha) > 28 ? (($arraydia[1] + $valorFecha) - 28) : ($arraydia[1] + $valorFecha)) . " " . ($hora + 2) . ":" . $minutos . ":" . $segundos;
+    $estadoDespachador .= "INSERT INTO estadoDespachador VALUES('" . $idEstadoDespachador . "','" . $fechaED . "','5','" . $i . "','" . $idDespachador . "');<br>";
+
+    /**comentarioDespachador1 */
+    $bandEstado = rand(1, 2);
+    if ($bandEstado == 1) {
+        $AC = rand(0, 4);
+        $comentarioDespachador = $comentarioDespachador . "INSERT INTO comentarioDespachador('','" . $fechaED . "','" . $arrayCDE1[$AC] . "','" . $idEstadoDespachador . "'); <br>";
+    }
+
+    $idEstadoDespachador++;
+
+    #estadodESPACHADOR2
+    $fechaED = "2020-0" . (($arraydia[1] + $valorFecha) > 28 ? $mes + 1 : $mes) . "-" . (($arraydia[1] + $valorFecha) < 10 ? "0" : "") . "" . (($arraydia[1] + $valorFecha) > 28 ? (($arraydia[1] + $valorFecha) - 28) : ($arraydia[1] + $valorFecha)) . " " . ($hora + 3) . ":" . $minutos . ":" . $segundos;
+    $estadoDespachador .= "INSERT INTO estadoDespachador VALUES('" . $idEstadoDespachador . "','" . $fechaED . "','6','" . $i . "','" . $idDespachador . "');<br>";
+
+    /**comentarioDespachador2 */
+    $bandEstado = rand(1, 2);
+    if ($bandEstado == 1) {
+        $AC = rand(0, 4);
+        $comentarioDespachador = $comentarioDespachador . "INSERT INTO comentarioDespachador('','" . $fechaED . "','" . $arrayCDE2[$AC] . "','" . $idEstadoDespachador . "'); <br>";
+    }
+
+    $idEstadoDespachador++;
+
+    #estadodESPACHADOR3
+    $hora = 8;
+    $minutos = rand(1, 59);
+    $segundos = rand(1, 59);
+    $fechaED = "2020-0" . (($arraydia[1] + $valorFecha + 1) > 28 ? ($mes + 1) : $mes) . "-" . (($arraydia[1] + $valorFecha + 1) < 10 ? "0" : "") . "" . (($arraydia[1] + $valorFecha + 1) > 28 ? "0".(($arraydia[1] + $valorFecha + 1) - 28) : ($arraydia[1] + $valorFecha + 1)) . " " . ($hora + 3) . ":" . $minutos . ":" . $segundos;
+    $estadoDespachador .= "INSERT INTO estadoDespachador VALUES('" . $idEstadoDespachador . "','" . $fechaED . "','7','" . $i . "','" . $idDespachador . "');<br>";
+
+    /**comentarioDespachador3 */
+    $bandEstado = rand(1, 2);
+    if ($bandEstado == 1) {
+        $AC = rand(0, 1);
+        $comentarioDespachador = $comentarioDespachador . "INSERT INTO comentarioDespachador('','" . $fechaED . "','" . $arrayCDE3[$AC] . "','" . $idEstadoDespachador . "'); <br>";
+    }
+
+    $idEstadoDespachador++;
+
+    /*AUNMENTO DIA */
+    if ($arraydia[0] == 1 and $arraydia[1] == 18) {
+        $arraydia[0] = 2;
+        $arraydia[1] = 1;
+        $mes = 7;
+    } else if ($arraydia[0] == 2 and $arraydia[1] == 21) {
+        $arraydia[0] = 3;
+        $arraydia[1] = 1;
+        $mes = 8;
+    }
+    $arraydia[1] = $arraydia[1] + 1;
+}
+echo $citas;
+echo $envio;
+echo $info;
+echo $estadoCliente;
+echo $comentarioCliente;
+echo $estadoConductor;
+echo $comentarioConductor;
+echo $estadoDespachador;
+echo $comentarioDespachador;
+
